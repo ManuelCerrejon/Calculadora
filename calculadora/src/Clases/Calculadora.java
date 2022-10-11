8abeacf568059b1c71c6cbcd66e94f8c81ce9cbd
usuario <usuario@pc22-038>
2022-10-11 10:20:59 +0200

    Primer Commit

---
+++ calculadora/src/Clases/Calculadora.java
@@ -0,0 +1,26 @@
+package Clases;
+
+public class Calculadora {
+	
+	public int suma(int a, int b) {
+		return a+b;
+	}
+	
+	public int resta(int a, int b) {		
+		return a-b;
+	}
+	
+	public int multiplicacion(int a, int b) { 
+		return a*b;
+	}
+	
+	public int division(int a, int b) {
+		return a/b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
