57767441e3303d6bb2745ce77d4090549408a1f0
usuario <usuario@pc22-038>
2022-10-11 10:38:43 +0200

    Clase Test incluida

--- calculadora/src/Clases/Calculadora.java
+++ calculadora/src/Clases/Calculadora.java
@@ -2,20 +2,26 @@ package Clases;
 
 public class Calculadora {
 	
-	public int suma(int a, int b) {
+	public static int suma(int a, int b) {
 		return a+b;
 	}
 	
-	public int resta(int a, int b) {		
+	public static int resta(int a, int b) {		
 		return a-b;
 	}
 	
-	public int multiplicacion(int a, int b) { 
+	public static int multiplicacion(int a, int b) { 
 		return a*b;
 	}
 	
-	public int division(int a, int b) {
-		return a/b;
+	public static int division(int a, int b) {
+		if (b==0)
+		{
+			return -1;
+		}else {
+			return a/b;
+		}
+		
 	}
 
 	public static void main(String[] args) {
